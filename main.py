a1429ef0597d3d305ab92ba33f623f9bf4b506b5
mohamed adel <muhammedadeltaha@gmail.com>
2023-11-21 03:25:03 +0200

    Adding test cases

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import time
-from search import goal_state, bfs, dfs, a_star_with_manhattan, a_star_with_euclidean, print_path
+from search import goal_state, dfs, bfs, a_star_with_manhattan, a_star_with_euclidean, print_path
 
 
-def display_results(solution, time_taken):
-    parents, expanded_nodes, depth = solution
+def get_path(parents):
     if parents is not None:
         state = goal_state
         path = [state]
@@ -11,104 +10,108 @@ def display_results(solution, time_taken):
             state = parents[state]
             path.append(state)
 
+        return path
+    return None
+
+
+def search_results(search_technique, initial_state: tuple):
+    start = time.time()
+    parents, expanded_nodes, depth = search_technique(initial_state)
+    running_time = time.time() - start
+    path = get_path(parents)
+    return path, depth, expanded_nodes, running_time
+
+
+def display_results_on_terminal(search_technique, test):
+    path, depth, expanded_nodes, running_time = search_results(search_technique, test)
+
+    if path is not None:
         print_path(path)
         print("Cost of the path:", len(path) - 1)
     else:
         print("Not Found")
-
     print("Depth:", depth)
     print("Number of expanded nodes:", expanded_nodes)
-    print("Running time:", time_taken)
+    print("Running time:", running_time)
 
 
-def bfs_test(initial_state: tuple):
-    print("---------------------------")
-    print("Using Breadth First Search:")
-    print("---------------------------")
-    start = time.time()
-    sol = bfs(initial_state)
-    end = time.time()
-    display_results(sol, end - start)
-
-
-def dfs_test(initial_state: tuple):
+def test_on_terminal(test):
     print("-------------------------")
     print("Using Depth First Search:")
     print("-------------------------")
-    start = time.time()
-    sol = dfs(initial_state)
-    end = time.time()
-    display_results(sol, end - start)
+    display_results_on_terminal(dfs, test)
 
+    print("---------------------------")
+    print("Using Breadth First Search:")
+    print("---------------------------")
+    display_results_on_terminal(bfs, test)
 
-def a_star_with_manhattan_test(initial_state: tuple):
     print("-------------------------")
     print("A* Search with manhattan:")
     print("-------------------------")
-    start = time.time()
-    sol = a_star_with_manhattan(initial_state)
-    end = time.time()
-    display_results(sol, end - start)
+    display_results_on_terminal(a_star_with_manhattan, test)
 
-
-def a_star_with_euclidean_test(initial_state: tuple):
     print("-------------------------")
     print("A* Search with euclidean:")
     print("-------------------------")
-    start = time.time()
-    sol = a_star_with_euclidean(initial_state)
-    end = time.time()
-    display_results(sol, end - start)
+    display_results_on_terminal(a_star_with_euclidean, test)
+
+
+def write_results_on_file(output_file, search_technique, test):
+    path, depth, expanded_nodes, running_time = search_results(search_technique, test)
+
+    if path is not None:
+        output_file.write("Cost of the path: " + str(len(path) - 1) + '\n')
+    else:
+        output_file.write("Not Found" + '\n')
+    output_file.write("Depth: " + str(depth) + '\n')
+    output_file.write("Number of expanded nodes: " + str(expanded_nodes) + '\n')
+    output_file.write("Running time: " + str(running_time) + '\n')
+
+
+def test_on_file(input_file_name, output_file_name):
+    with (open(input_file_name, 'rt', encoding='utf-8') as input_file,
+          open(output_file_name, 'wt', encoding='utf-8') as output_file
+          ):
+
+        for line in input_file:
+            test = line.strip()
+            if test == '':
+                continue
+
+            zero_idx = 8 - test.find('0')
+            num = int(test)
+            output_file.write('DFS\n')
+            write_results_on_file(output_file, dfs, (num, zero_idx, 0))
+            output_file.write('\nBFS\n')
+            write_results_on_file(output_file, bfs, (num, zero_idx, 0))
+            output_file.write('\nA* with Manhattan\n')
+            write_results_on_file(output_file, a_star_with_manhattan, (num, zero_idx, 0))
+            output_file.write('\nA* with Euclidean\n')
+            write_results_on_file(output_file, a_star_with_euclidean, (num, zero_idx, 0))
+            output_file.write('\n==============================================\n')
 
 
 # test case
 # ---------
-# bfs_test((123456870, 0, 0))
-# dfs_test((123456870, 0, 0))
-# a_star_with_manhattan_test((123456870, 0, 0))
-# a_star_with_euclidean_test((123456870, 0, 0))
+# test_on_terminal((123456870, 0, 0))
 # ------------------------------------------
-# bfs_test((125340678, 3, 0))
-# dfs_test((125340678, 3, 0))
-# a_star_with_manhattan_test((125340678, 3, 0))
-# a_star_with_euclidean_test((125340678, 3, 0))
+# test_on_terminal((125340678, 3, 0))
 # ------------------------------------------
-# bfs_test((142658730, 0, 0))
-# dfs_test((142658730, 0, 0))
-# a_star_with_manhattan_test((142658730, 0, 0))
-# a_star_with_euclidean_test((142658730, 0, 0))
+# test_on_terminal((142658730, 0, 0))
 # ------------------------------------------
-# bfs_test((102754863, 7, 0))
-# dfs_test((102754863, 7, 0))
-# a_star_with_manhattan_test((102754863, 7, 0))
-# a_star_with_euclidean_test((102754863, 7, 0))
+# test_on_terminal((102754863, 7, 0))
 # ------------------------------------------
-# bfs_test((806547231, 7, 0))
-# dfs_test((806547231, 7, 0))
-# a_star_with_manhattan_test((806547231, 7, 0))
-# a_star_with_euclidean_test((806547231, 7, 0))
+# test_on_terminal((806547231, 7, 0))
 # ------------------------------------------
-# bfs_test((312645078, 2, 0))
-# dfs_test((312645078, 2, 0))
-# a_star_with_manhattan_test((312645078, 2, 0))
-# a_star_with_euclidean_test((312645078, 2, 0))
+# test_on_terminal((312645078, 2, 0))
 # ------------------------------------------
-# bfs_test((102345678, 7, 0))
-# dfs_test((102345678, 7, 0))
-# a_star_with_manhattan_test((102345678, 7, 0))
-# a_star_with_euclidean_test((102345678, 7, 0))
+# test_on_terminal((102345678, 7, 0))
 # ------------------------------------------
-# bfs_test((312480657, 3, 0))
-# dfs_test((312480657, 3, 0))
-# a_star_with_manhattan_test((312480657, 3, 0))
-# a_star_with_euclidean_test((312480657, 3, 0))
+# test_on_terminal((312480657, 3, 0))
 # ------------------------------------------
-# bfs_test((538140276, 3, 0))
-# dfs_test((538140276, 3, 0))
-# a_star_with_manhattan_test((538140276, 3, 0))
-# a_star_with_euclidean_test((538140276, 3, 0))
+# test_on_terminal((538140276, 3, 0))
 # ------------------------------------------
-# bfs_test((821546037, 2, 0))
-# dfs_test((821546037, 2, 0))
-# a_star_with_manhattan_test((821546037, 2, 0))
-# a_star_with_euclidean_test((821546037, 2, 0))
+# test_on_terminal((821546037, 2, 0))
+# ------------------------------------------
+# test_on_file('input.txt', 'output.txt')
